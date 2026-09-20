ba9c69560ef807d730b9400671356e870ab699f6
segaharu <harukko15jp@gmail.com>
2026-09-20 10:54:14 +0900

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
 
   return (
     <main className="p-6">
-      <h1 className="mb-4 text-xl">あ</h1>
+      <h1 className="mb-4 text-xl">い</h1>
 
       {/* posts は配列。.map() で1件ずつ並べる */}
       {posts?.map((post) => (
